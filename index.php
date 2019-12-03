f1a497e067eab297176f9f92842820b14c1b9357
babazze <babazze@live.fr>
2019-12-03 11:48:06 +0100

    fini

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr" >
+  <head>
+    <meta charset="utf-8" />
+    <title></title>
+  </head>
+  <body>
+      <?php
+      function retourné ()
+      {
+          return "Chaine a retourner";
+      }
+      echo retourné ();
+      ?>
+  </body>
+</html>
